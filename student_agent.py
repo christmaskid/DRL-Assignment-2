eb5245a26c84c0b551b8b1a1d6f192d0460901b2
christmaskid <chengyt2001@gmail.com>
2025-03-29 23:36:46 +0800

    6-tuple td learning, 14000+ score

--- student_agent.py
+++ student_agent.py
@@ -231,21 +231,34 @@ class Game2048Env(gym.Env):
         # If the simulated board is different from the current board, the move is legal
         return not np.array_equal(self.board, temp_board)
 
-from mcts import *
+from q1 import NTupleApproximator
+import sys
+sys.modules['__main__'].NTupleApproximator = NTupleApproximator
+patterns = [
+        # https://ko19951231.github.io/2021/01/01/2048/
+        ((1, 0), (1, 1), (2, 0), (2, 1), (3, 0), (3, 1)),
+        ((1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2)),
+        ((0, 0), (1, 0), (2, 0), (2, 1), (3, 0), (3, 1)),
+        ((0, 1), (1, 1), (2, 1), (2, 2), (3, 1), (3, 2)),
+    ]
+approximator = NTupleApproximator(board_size=4, patterns=patterns)
+approximator = pickle.load(open("approximator.pkl", "rb"))
 
 def get_action(state, score):
     env = Game2048Env()
-    # return random.choice([0, 1, 2, 3]) # Choose a random action
+    env.board = state
+    env.score = score
     
-    uct_mcts = UCTMCTS(env, iterations=50, exploration_constant=1.41, rollout_depth=10)
-    root = UCTNode(env, state, score)  # Initialize the root node for MCTS
-    
-    # Run multiple simulations to construct and refine the search tree
-    for _ in range(uct_mcts.iterations):
-        uct_mcts.run_simulation(root)
+    legal_moves = [a for a in range(4) if env.is_move_legal(a)]
+    # shall not be empty
 
-    # Select the best action based on the visit distribution of the root's children
-    best_action, visit_distribution = uct_mcts.best_action_distribution(root)
-    # print("MCTS selected action:", best_action, "with visit distribution:", visit_distribution)
+    # TODO: Use your N-Tuple approximator to play 2048
+    action_values = []
+    for action in legal_moves:
+        sim_env = copy.deepcopy(env)
+        next_state, reward, _, _ = sim_env.step(action)
+        action_values.append(reward + approximator.value(next_state))
+    best_action = legal_moves[np.argmax(action_values)]
+
 
     return best_action
