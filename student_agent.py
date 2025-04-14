9e3d2d191e8ab4e81e8036eff030cb85b1e19fa6
christmaskid <chengyt2001@gmail.com>
2025-04-15 04:55:18 +0800

    Debug on the rollout and leaf node state

--- student_agent.py
+++ student_agent.py
@@ -2,53 +2,34 @@
 import pickle
 
 from approximator import NTupleApproximator
-from libenv2048.env2048 import Game2048Env
-from td_mcts_new import *
+from env2048 import Game2048Env
+# from td_mcts_new import *
+from td_mcts import *
 
 import sys
 sys.modules['__main__'].NTupleApproximator = NTupleApproximator
 patterns = [
-        # https://ko19951231.github.io/2021/01/01/2048/
-        ((1, 0), (1, 1), (2, 0), (2, 1), (3, 0), (3, 1)),
-        ((1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2)),
-        ((0, 0), (1, 0), (2, 0), (2, 1), (3, 0), (3, 1)),
-        ((0, 1), (1, 1), (2, 1), (2, 2), (3, 1), (3, 2)),
-    ]
+    # https://ko19951231.github.io/2021/01/01/2048/
+    ((1, 0), (1, 1), (2, 0), (2, 1), (3, 0), (3, 1)),
+    ((1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2)),
+    ((0, 0), (1, 0), (2, 0), (2, 1), (3, 0), (3, 1)),
+    ((0, 1), (1, 1), (2, 1), (2, 2), (3, 1), (3, 2)),
+]
 approximator = NTupleApproximator(board_size=4, patterns=patterns)
-approximator = pickle.load(open("submission/Q3/approximator_4_6_my_new.pkl", "rb"))
+approximator = pickle.load(open("approximator_4_6_my_new.pkl", "rb"))
 
 def get_action(state, score):
-    print("Approx", approximator.value(state), score, flush=True)
+    # print("Approx", approximator.value(state), score, flush=True)
     env = Game2048Env()
-    env.board = state
+    env.board = state.copy()
     env.score = score
         
-    td_mcts = TD_MCTS(env, approximator, iterations=50, exploration_constant=1.41 * 0.1, rollout_depth=0, gamma=0.99)
-    root = TD_MCTS_Node(env, state, score)
-
+    td_mcts = TD_MCTS(env, approximator, iterations=50, exploration_constant=0, rollout_depth=0, gamma=0.99)
+    root = TD_MCTS_Node(env, state.copy(), score)
+ 
     # Run multiple simulations to build the MCTS tree
     for _ in range(td_mcts.iterations):
         td_mcts.run_simulation(root)
-
-    for child in root.children.values():
-        q = child.total_reward
-        if child.visits == 0:
-            print(child.action,"q", q, 0)
-            afterstate = child.state
-        else:
-            uct_value = q + td_mcts.c * math.sqrt(math.log(root.visits) / child.visits)
-            print(child.action, child.score, "total_reward (q)", q, "explore_term", uct_value-q, "uct", uct_value)
-            test_env = td_mcts.create_env_from_state(child.afterstate, child.score)
-            _, afterstate, _, done, _ = test_env.step(child.action)
-            print("afterstate", child.action, "\n", afterstate)
-            print("approx", approximator.value(afterstate), done)
-            
-        print("td choice value", child.action, approximator.value(afterstate) + child.score)
-
-        for grandchild in child.children.values():
-            print(grandchild.action, end=" ")
-        print()
-
 
     # Select the best action (based on highest visit count)
     best_act, _ = td_mcts.best_action_distribution(root)
@@ -56,34 +37,3 @@ def get_action(state, score):
     print(best_act, _, flush=True)
     return best_act
 
-
-
-# Approx 13819.517009951702 5844
-# 0 q 21746.367384998353 -> 1 explore_term 0.4111884402736905 uct 21746.778573438627 -> 1.41
-# 1 q 13953.972208943396 -> 0 explore_term 1.3944078435924894 uct 13955.366616786989 -> 1.39
-# Approx 29860.85746170198 908
-# [[ 2  2 16 32]
-#  [ 8 16 32 64]
-#  [ 4  8 16 32]
-#  [ 2  4  8 16]]
-# Score 908
-# 3 [0.   0.   0.08 0.92]
-"""
-Approx 13251.624305283207 2616
-(0, 3, 2)
-[[  2   4  64   2]
- [256  16  32   8]
- [ 64   4  16   2]
- [  4   2   4   2]]
-Score 2616
-0 [0.92 0.08 0.   0.  ]
-Agent finished in 222 steps, Score: 2620
-
-[TD-MCTS]
-0 total_reward (q) 17249.587902423576 explore_term 0.4111884402736905 uct 17249.99909086385
-1 total_reward (q) 13854.122155882706 explore_term 1.3944078435924894 uct 13855.516563726298
-
-[PURE TD]
-0 td choice value 0 12274.252376059136
-1 td choice value 1 13920.384764671197
-"""
--- td_mcts.py
+++ td_mcts.py
@@ -1,17 +1,12 @@
-# ==== VERSION: DEALING WITH STOCHASITY BY EXPANDING TWO LAYERS OF NODES. VER.2 ==== #
-
 import copy
 import random
 import math
 import numpy as np
+MAX_RAND_NODES=4
 
-MIN_RAND_NODES=5
-Q_NORM_VALUE=1 #30 #1 #0.5
-
-# Node for TD-MCTS using the TD-trained value approximator
 class TD_MCTS_Node:
-    def __init__(self, env ,state, score, action=None, parent=None):
-        self.state = state
+    def __init__(self, env, state, score, action=None, parent=None):
+        self.state = state.copy()
         self.score = score
         self.parent = parent
         self.action = action
@@ -23,8 +18,8 @@ class TD_MCTS_Node:
         self.untried_actions = [a for a in range(4) if env.is_move_legal(a)]
 
     def fully_expanded(self):
-        return len(self.untried_actions) == 0 and len(self.children)>0
-
+        return len(self.untried_actions) == 0
+    
 class After_Node:
     def __init__(self, afterstate, score, action=None, parent=None):
         self.afterstate = afterstate
@@ -36,14 +31,14 @@ class After_Node:
         self.total_reward = 0.0
 
         self.children = {}
-        self.untried_actions_num = min(len(np.where(afterstate==0)[0]), MIN_RAND_NODES)
+        self.untried_action_num = min(len(np.where(afterstate==0)[0])*2, MAX_RAND_NODES)
 
     def fully_expanded(self):
-      return self.untried_actions_num==0 and len(self.children)>0
-
-
+        return self.untried_action_num==0
+    
 class TD_MCTS:
-    def __init__(self, env, approximator, iterations=500, exploration_constant=1.41, rollout_depth=10, gamma=0.99):
+    def __init__(self, env, approximator, iterations, exploration_constant,
+                 rollout_depth, gamma=0.99):
         self.env = env
         self.approximator = approximator
         self.iterations = iterations
@@ -56,144 +51,127 @@ class TD_MCTS:
         new_env.board = state.copy()
         new_env.score = score
         return new_env
-
+    
     def select_random_tile(self, node):
-        empty_cells = list(zip(*np.where(node.afterstate == 0)))
-        if not empty_cells:
-          return None
+        empty_cells = list(zip(*np.where(node.afterstate==0)))
+        if len(empty_cells)==0: # should not happen
+            return None
         x, y = random.choice(empty_cells)
         tile = 2 if random.random() < 0.9 else 4
-        return (x,y,tile)
-
+        return (x, y, tile)
+    
     def select_child(self, node):
         if isinstance(node, TD_MCTS_Node):
-          best_score = -float('inf')
-          selected_child = None
+            best_score = -float('inf')
+            selected_child = None
 
-          q_values = dict()
-          for child in node.children.values():
-            if child.visits > 0:
-              q_values[child] = child.total_reward
-          q_values_avg = np.mean(list(q_values.values()))
-          min_q, max_q = min(q_values.values()), max(q_values.values())
+            q_values = []
+            for child in node.children.values():
+                if child.visits == 0:
+                    return child
+                q_values.append(child.total_reward)
+            min_q, max_q = min(q_values), max(q_values)
 
-          for child in node.children.values():
-            if child.visits == 0:
-              return child
-              # q = q_values_avg
+            for child in node.children.values():
+                q = child.total_reward
+                # here child.visits > 0 must held
+                uct_value = q + self.c * math.sqrt(math.log(node.visits) / child.visits)
+                if uct_value > best_score:
+                    best_score, selected_child = uct_value, child
+
+            return selected_child
+
+        elif isinstance(node, After_Node):
+            keys = node.children.keys()
+            twos = [item for item in keys if item[2] == 2]
+            fours = [item for item in keys if item[2] == 4]
+            if len(twos) == 0:
+                action = random.choice(fours)
+            elif len(fours) == 0:
+                action = random.choice(twos)
+            elif random.random() < 0.9:
+                action = random.choice(twos)
             else:
-              q = q_values[child]
-
-            # Normalize
-            if max_q==min_q:
-              q = 0
-            else:
-              q = (q-min_q)/(max_q-min_q) * Q_NORM_VALUE
-
-            uct_value = q + self.c * math.sqrt(math.log(node.visits) / child.visits)
-            # print("q", q, "explore_term", uct_value-q, "uct", uct_value)
-
-            if uct_value > best_score:
-              best_score, selected_child = uct_value, child
-
-          selected_action = selected_child.action
+                action = random.choice(fours)
+            return node.children[action]
 
         else:
-          best_score = -float('inf')
-          selected_child = random.choice(list(node.children.values()))
-
-        # print(node, selected_child)#, selected_action)
-        return selected_child
-
-    def rollout(self, sim_env, depth):
-        afterstate = sim_env.board
-        while depth>0:
-          legal_moves = [a for a in range(4) if sim_env.is_move_legal(a)]
-          if not legal_moves:
-            break
-          action = random.choice(legal_moves)
-          _, afterstate, _, _, _ = sim_env.step(action)
-          depth -= 1
-        # print(self.approximator.value(sim_env.board), np.max(sim_env.board))
-        return self.approximator.value(sim_env.board) + sim_env.score
-
-    # def rollout(self, sim_env, depth):
-    #   while depth > 0:
-    #       legal_moves = [a for a in range(4) if sim_env.is_move_legal(a)]
-    #       if not legal_moves:
-    #           break
-    #       best_score = -float('inf')
-    #       best_action = None
-    #       for a in legal_moves:
-    #           test_env = copy.deepcopy(sim_env)
-    #           _, afterstate, next_score, _, _ = test_env.step(a)
-    #           reward = next_score - sim_env.score
-    #           value = reward + self.gamma * self.approximator.value(afterstate)
-    #           if value > best_score:
-    #               best_score = value
-    #               best_action = a
-    #       _, _, _, _, _ = sim_env.step(best_action)
-    #       depth -= 1
-    #   return self.approximator.value(sim_env.board) + sim_env.score
+            print("Wrong node type: {}".format(type(node)))
+            raise Exception
 
 
+    def rollout(self, env, depth, afterstate):
+        # # here the state of env must be an afterstate
+        # afterstate = env.board.copy()
+        done = env.is_game_over()
+
+        while depth>0 and not done:
+            legal_moves = [a for a in range(4) if env.is_move_legal(a)]
+            if len(legal_moves)==0:
+                done = True
+                break
+            action = random.choice(legal_moves)
+            _, afterstate, _, done, _ = env.step(action, True)
+            depth -= 1
+
+        if done:
+            return env.score
+        else:
+            return self.approximator.value(afterstate) + env.score
+        
     def backpropagate(self, node, reward):
-        child = node
         while node:
-          node.visits += 1
-          # node.total_reward += (reward - node.total_reward) / node.visits
-          # if child != node: # the reward of this step from node
-          #   action_reward = child.score - node.score
-          # else:
-          #   action_reward = 0
-          # node.total_reward += (reward + action_reward - node.total_reward) / node.visits
-          # child = node
-          node.total_reward += (reward - node.total_reward) / node.visits
-          reward *= self.gamma # decreasing
-          node = node.parent
+            node.visits += 1
+            node.total_reward += (reward - node.total_reward) / node.visits
+            node = node.parent
 
     def run_simulation(self, root):
+        # assert isinstacne(root, TD_MCTS_Node)
         node = root
         sim_env = self.create_env_from_state(node.state, node.score)
-
-        # Selection
+        # 1. Selection
         done = False
-        while node.fully_expanded():
-          node = self.select_child(node)
-          # print("Select", node, node.action)
-          if isinstance(node, After_Node):
-            _, afterstate, next_score, done, _ = sim_env.step(node.action)
-          else:
-            i, j, tile = node.action
-            afterstate = sim_env.board.copy()
-            afterstate[i, j] = tile
-            sim_env.board = afterstate.copy()
-          # if done:
-          #   break
+        while node.fully_expanded() and not done:
+            node = self.select_child(node)
+            if isinstance(node, TD_MCTS_Node):
+                # Now place THE random tile on the board
+                r, c, tile = node.action
+                sim_env.board[r, c] = tile
+                done = sim_env.is_game_over()
+            elif isinstance(node, After_Node):
+                # Now move the board according to the action
+                _, afterstate, _, done, _ = sim_env.step(node.action, False)
+            else:
+                print("Wrong node type: {}".format(node), flush=True)
+                raise Exception
+        
+        # 2. Expansion
+         # TD_MCTS_Node -> action -> After_Node -> add random tile -> TD_MCTS_Node -> ...
+        if not done and not node.fully_expanded():
 
-        # Expand
-        if not node.fully_expanded():
-          if isinstance(node, TD_MCTS_Node) and len(node.untried_actions)>0: # select an action
-            action = random.choice(node.untried_actions)
-            node.untried_actions.remove(action)
-            _, afterstate, next_score, _, _ = sim_env.step(action)
-            node.children[action] = After_Node(afterstate=afterstate, score=next_score, parent=node, action=action)
-            # print("Expand an action:", node, action, node.children[action])
-            node = node.children[action]
+            if isinstance(node, TD_MCTS_Node):
+                action = random.choice(node.untried_actions)
+                node.untried_actions.remove(action)
+                _, afterstate, next_score, done, _ = sim_env.step(action, False)
+                node.children[action] = After_Node(afterstate, next_score, action=action, parent=node)
+                node = node.children[action]
+            
+            elif isinstance(node, After_Node):
+                action = self.select_random_tile(node)
+                node.untried_action_num -= 1
+                r, c, tile = action
+                afterstate = sim_env.board.copy()
+                sim_env.board[r, c] = tile
+                done = sim_env.is_game_over()
+                node.children[action] = TD_MCTS_Node(sim_env, sim_env.board.copy(), 
+                                                     sim_env.score, action=action, parent=node)
+                node = node.children[action]
 
-          elif isinstance(node, After_Node) and node.untried_actions_num>0: # add an random tile
-            action = self.select_random_tile(node)
-            node.untried_actions_num -= 1
-            i, j, tile = action
-            sim_env.board[i, j] = tile
-            node.children[action] = TD_MCTS_Node(env=sim_env, state=sim_env.board.copy(), score=sim_env.score, parent=node, action=action)
-            # print("Expand a random tile:", node, action, node.children[action])
-            node = node.children[action]
-
-        rollout_reward = self.rollout(sim_env, self.rollout_depth) #- root.score
-        # rollout_reward = self.approximator.value(sim_env.board) + sim_env.score - root.score
+        # 3. Rollout (or not) and 4. Backpropagation
+        # if isinstance(node, After_Node):
+        rollout_reward = self.rollout(sim_env, self.rollout_depth, afterstate)
         self.backpropagate(node, rollout_reward)
-
+                
     def best_action_distribution(self, root):
         total_visits = sum(child.visits for child in root.children.values())
         distribution = np.zeros(4)
@@ -203,3 +181,5 @@ class TD_MCTS:
             distribution[action] = child.visits / total_visits if total_visits > 0 else 0
 
         return best_action, distribution
+                
+                
